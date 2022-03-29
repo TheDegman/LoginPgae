601194e30ea3fd38ea3e1bea9a7b7481a719beff
Dorian Marušić <dorian.marusic@skole.hr>
2022-03-29 09:08:15 +0200

    .

--- LoginPgae/Form1.Designer.cs
+++ LoginPgae/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LoginPgae
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,58 +29,245 @@ namespace LoginPgae
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.LoginPanel = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RememberMe = new System.Windows.Forms.CheckBox();
+            this.LoginUsername = new LoginPgae.Controls.TextBox();
+            this.LoginPassword = new LoginPgae.Controls.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new LoginPgae.Controls.TextBox();
+            this.textBox2 = new LoginPgae.Controls.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.LoginPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // LoginPanel
             // 
             this.LoginPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(122)))), ((int)(((byte)(145)))));
-            this.LoginPanel.Controls.Add(this.textBox2);
-            this.LoginPanel.Controls.Add(this.textBox1);
             this.LoginPanel.Controls.Add(this.panel1);
+            this.LoginPanel.Controls.Add(this.linkLabel1);
+            this.LoginPanel.Controls.Add(this.label1);
+            this.LoginPanel.Controls.Add(this.RememberMe);
+            this.LoginPanel.Controls.Add(this.LoginUsername);
+            this.LoginPanel.Controls.Add(this.LoginPassword);
+            this.LoginPanel.Controls.Add(this.LoginBtn);
             this.LoginPanel.Location = new System.Drawing.Point(332, 126);
             this.LoginPanel.Name = "LoginPanel";
             this.LoginPanel.Size = new System.Drawing.Size(312, 308);
             this.LoginPanel.TabIndex = 0;
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.ForeColor = System.Drawing.Color.White;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(126, 260);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(69, 15);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Register Me";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(112, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign In";
+            // 
+            // RememberMe
+            // 
+            this.RememberMe.AutoSize = true;
+            this.RememberMe.BackColor = System.Drawing.Color.Transparent;
+            this.RememberMe.ForeColor = System.Drawing.Color.White;
+            this.RememberMe.Location = new System.Drawing.Point(106, 173);
+            this.RememberMe.Name = "RememberMe";
+            this.RememberMe.Size = new System.Drawing.Size(104, 19);
+            this.RememberMe.TabIndex = 9;
+            this.RememberMe.Text = "Remember Me";
+            this.RememberMe.UseVisualStyleBackColor = false;
+            // 
+            // LoginUsername
+            // 
+            this.LoginUsername.BackColor = System.Drawing.Color.White;
+            this.LoginUsername.BorderColor = System.Drawing.Color.Navy;
+            this.LoginUsername.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.LoginUsername.BorderSize = 4;
+            this.LoginUsername.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoginUsername.Location = new System.Drawing.Point(43, 82);
+            this.LoginUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginUsername.Multiline = false;
+            this.LoginUsername.Name = "LoginUsername";
+            this.LoginUsername.Padding = new System.Windows.Forms.Padding(7);
+            this.LoginUsername.PasswordChar = false;
+            this.LoginUsername.Size = new System.Drawing.Size(227, 32);
+            this.LoginUsername.TabIndex = 1;
+            this.LoginUsername.Texts = "Username";
+            this.LoginUsername.UnderlinedStyle = true;
+            // 
+            // LoginPassword
+            // 
+            this.LoginPassword.BackColor = System.Drawing.Color.White;
+            this.LoginPassword.BorderColor = System.Drawing.Color.Navy;
+            this.LoginPassword.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.LoginPassword.BorderSize = 4;
+            this.LoginPassword.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoginPassword.Location = new System.Drawing.Point(43, 125);
+            this.LoginPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginPassword.Multiline = false;
+            this.LoginPassword.Name = "LoginPassword";
+            this.LoginPassword.Padding = new System.Windows.Forms.Padding(7);
+            this.LoginPassword.PasswordChar = false;
+            this.LoginPassword.Size = new System.Drawing.Size(227, 32);
+            this.LoginPassword.TabIndex = 8;
+            this.LoginPassword.Texts = "Password";
+            this.LoginPassword.UnderlinedStyle = true;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.White;
+            this.LoginBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.LoginBtn.FlatAppearance.BorderSize = 0;
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBtn.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoginBtn.Location = new System.Drawing.Point(58, 207);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(202, 32);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(97)))));
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(122)))), ((int)(((byte)(145)))));
+            this.panel1.Controls.Add(this.linkLabel2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(312, 64);
-            this.panel1.TabIndex = 1;
+            this.panel1.Size = new System.Drawing.Size(312, 308);
+            this.panel1.TabIndex = 11;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.ForeColor = System.Drawing.Color.White;
+            this.linkLabel2.LinkColor = System.Drawing.Color.White;
+            this.linkLabel2.Location = new System.Drawing.Point(126, 260);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(69, 15);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Register Me";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(112, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sign Up";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(106, 173);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Remember Me";
+            this.checkBox1.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 82);
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderColor = System.Drawing.Color.Navy;
+            this.textBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox1.BorderSize = 4;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(43, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = false;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox1.PasswordChar = false;
+            this.textBox1.Size = new System.Drawing.Size(227, 32);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Texts = "Username";
+            this.textBox1.UnderlinedStyle = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(113, 127);
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderColor = System.Drawing.Color.Navy;
+            this.textBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox2.BorderSize = 4;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(43, 125);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = false;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox2.PasswordChar = false;
+            this.textBox2.Size = new System.Drawing.Size(227, 32);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Texts = "Password";
+            this.textBox2.UnderlinedStyle = true;
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(58, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(4)))), ((int)(((byte)(76)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1009, 556);
             this.Controls.Add(this.LoginPanel);
-            this.Name = "Form1";
+            this.Name = "Login";
             this.Text = "Form1";
             this.LoginPanel.ResumeLayout(false);
             this.LoginPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -88,9 +275,19 @@ namespace LoginPgae
         #endregion
 
         private System.Windows.Forms.Panel LoginPanel;
+        private System.Windows.Forms.Button LoginBtn;
+        private Controls.TextBox LoginUsername;
+        private Controls.TextBox LoginPassword;
+        private System.Windows.Forms.CheckBox RememberMe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private Controls.TextBox textBox1;
+        private Controls.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- LoginPgae/Form1.cs
+++ LoginPgae/Form1.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LoginPgae
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
         
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+            LoginPanel.BackColor= Color.FromArgb(125,Color.Black);
             
+            LoginBtn.BackColor = Color.FromArgb(125, Color.White);
 
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
 
         }
     }
